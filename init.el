df74c0ffd6438b20faceb5fb370c09567a52007a
Neil <jingsi.chen@qq.com>
2018-05-23 20:40:47 +0800

    add my init

---
+++ init.el
@@ -0,0 +1,10 @@
+;;;; Neil's emacs config
+;; Neil Chan
+;; 23 May 2018
+
+;;;; The Help Key
+;; Control-h is the help key;
+;; after typing control-h, type a letter to
+;; indicate the subject about which you want help.
+;; For an explanation of the help facility,
+;; type control-h two times in a row.
