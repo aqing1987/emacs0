182f9dfa1dac6e088c6c8d0ce781ccd31cb25d3e
Neil <jingsi.chen@qq.com>
2018-05-24 20:43:37 +0800

    ui: disable menubar, toolbar

--- init.el
+++ init.el
@@ -1,10 +1,16 @@
-;;;; Neil's emacs config
+;;; Neil's emacs config
 ;; Neil Chan
 ;; 23 May 2018
 
-;;;; The Help Key
+;;; The Help Key
 ;; Control-h is the help key;
 ;; after typing control-h, type a letter to
 ;; indicate the subject about which you want help.
 ;; For an explanation of the help facility,
 ;; type control-h two times in a row.
+
+;; add directories to search when loading files with `load'
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+
+(require 'init-ui)
+
---
+++ lisp/init-ui.el
@@ -0,0 +1,8 @@
+
+(provide 'init-ui)
+
+;; disable tool bar
+(tool-bar-mode -1)
+
+;; disable menu bar
+(menu-bar-mode -1)
