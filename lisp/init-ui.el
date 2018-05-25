a808f85cc7af84914339e3110c2a79e5d6313929
Neil <jingsi.chen@qq.com>
2018-05-25 17:04:16 +0800

    ui: change startup screen

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,8 +1,16 @@
 
 (provide 'init-ui)
+;;; $
 
 ;; disable tool bar
 (tool-bar-mode -1)
 
 ;; disable menu bar
 (menu-bar-mode -1)
+
+;; disable startup screen
+(setq-default inhibit-startup-screen t)
+
+;; change scratch message
+(setq-default initial-scratch-message ";; Happy Hacking!\n\n")
+
