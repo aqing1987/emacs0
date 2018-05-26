0bf8c8ef4eab6c8281760bfe1ad5b242535110d1
Kevin <jingsi.chen@qq.com>
2018-05-26 19:10:23 +0800

    ui: set default font as Inconsolata

--- init.el
+++ init.el
@@ -2,13 +2,6 @@
 ;; Neil Chan
 ;; 23 May 2018
 
-;;; The Help Key
-;; Control-h is the help key;
-;; after typing control-h, type a letter to
-;; indicate the subject about which you want help.
-;; For an explanation of the help facility,
-;; type control-h two times in a row.
-
 ;; add directories to search when loading files with `load'
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,7 +1,4 @@
 
-(provide 'init-ui)
-;;; $
-
 ;; disable tool bar
 (tool-bar-mode -1)
 
@@ -23,3 +20,11 @@
 ;; config theme
 (load-theme 'leuven t)
 
+;;; change default font
+;; check current font: (face-attribute 'default :font)
+;; download Inconsolata if not exist
+(add-to-list 'default-frame-alist
+	     '(font . "Inconsolata-18"))
+
+;;; $
+(provide 'init-ui)
