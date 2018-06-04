9c818cc0e2c9955e2aa26182b79a376cb924eef8
Neil <jingsi.chen@qq.com>
2018-06-04 09:27:22 +0800

    company-c-headers: add complete c++ header files support

--- custom/setup-company.el
+++ custom/setup-company.el
@@ -12,7 +12,9 @@
 
 (use-package company-c-headers
   :init
-  (add-to-list 'company-backends 'company-c-headers))
+  (add-to-list 'company-backends 'company-c-headers)
+  :config
+  (add-to-list 'company-c-headers-path-system "/usr/include/c++/5/"))
 
 (provide 'setup-company)
 ;;; setup-company.el ends here
