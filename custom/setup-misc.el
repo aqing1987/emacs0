f6c4ac9432b204947bd61002ed5a32fa5cff6c2d
Neil <jingsi.chen@qq.com>
2018-06-04 11:31:45 +0800

    add misc: hungry-delete

---
+++ custom/setup-misc.el
@@ -0,0 +1,12 @@
+;;; package --- summary:
+;;; commentary:
+
+;;; code:
+
+;; deletes all the whitespace when you hit backspace or delete
+(use-package hungry-delete
+  :config
+  (global-hungry-delete-mode))
+
+(provide 'setup-misc)
+;;; setup-misc.el ends here
