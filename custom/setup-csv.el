aaad431535d018c325e4233a437ee7d05de5c5d2
Neil <jingsi.chen@qq.com>
2018-06-25 09:52:32 +0800

    csv: add csv-separators

--- custom/setup-csv.el
+++ custom/setup-csv.el
@@ -6,7 +6,8 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
   (autoload 'csv-mode "csv-mode"
-    "Major mode for editing comma-separated value files." t))
+    "Major mode for editing comma-separated value files." t)
+  (setq csv-separators '("," ";" "|" " ")))
 
 (provide 'setup-csv)
 ;;; setup-csv.el ends here
